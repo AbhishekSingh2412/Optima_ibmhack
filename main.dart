617fb70ed5c3ecac0586fd93b39030e479a5c7ae
AbhishekSingh2412 <56639395+AbhishekSingh2412@users.noreply.github.com>
2020-05-07 11:12:08 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:optimaibmhack/OwnerOrBuyer.dart';
 import 'LoginAndRegPage.dart';
 import 'login_screen.dart';
 import 'story_screen.dart';
 import 'registration_screen.dart';
 import 'welcome_screen.dart';
+import 'OwnerOrBuyer.dart';
+import 'success.dart';
 void main() {
   runApp(MyApp());
 }
@@ -16,10 +19,13 @@ class MyApp extends StatelessWidget {
       initialRoute: 'welcome_screen' ,
       routes: {
        'welcome_screen':(context)=> WelcomeScreen(),
+        'OwnerOrBuyer':(context)=>OwnerOrBuyer(),
         'registration_screen':(context)=>RegistrationScreen(),
         'login_screen':(context)=>LoginScreen(),
         'story_screen':(context)=>StoryScreen(),
        'LoginAndRegPage':(context)=>LoginAndRegPage(),
+        'success':(context)=>Success(),
+
 
       },
     );
